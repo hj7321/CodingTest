20b3c05fd2f1f2799d61383be7924e81bd92c717
hj7321 <oboki2001@naver.com>
2024-06-02 10:55:10 +0900

    문제 푸는 중

--- programmers/level1/00_1_공원 산책.js
+++ programmers/level1/00_1_공원 산책.js
@@ -1,6 +1,28 @@
 // 연습문제 - 공원 산책
 
+// E: 오른쪽으로 이동(열 이동)
+// "E 2" : [0, 0] => [0, 2]
+// W: 왼쪽으로 이동(열 이동)
+// "W 2" : [0, 0] => [0, -2]
+// S: 아래쪽으로 이동(행 이동)
+// "S 2" : [0, 0] => [2, 0]
+// N: 위쪽으로 이동(행 이동)
+// "N 2" : [0, 0] => [-2, 0]
+
+// 시작 지점의 좌표를 구함
+// routes 배열을 순회하면서 좌표로 이동하면 그 곳이 park에서 O인지 X인지 판단
+// O면 이동, X면 그대로 있기
+
 function solution(park, routes) {
-  var answer = [];
+  let answer = [];
+  let start;
+  park.forEach((str, x) => {
+    let y = str.indexOf("S");
+    if (y !== -1) start = [x, y];
+  });
+  routes.reduce((end, route) => {
+    route[0];
+    return end;
+  }, start);
   return answer;
 }
--- programmers/level1/00_4_햄버거 만들기.js
+++ programmers/level1/00_4_햄버거 만들기.js
@@ -6,6 +6,7 @@
 // 배열 ingredient를 문자열로 만들고, 그 후에 "1231"이 있는 첫 번째 부분의 인덱스를 찾아서 그 인덱스에 해당하는 요소부터 4개를 slice로 자름
 // 정규식을 이용하자
 // 1"1231"2"1231"3"1231"1
+// 112231
 
 // 시간 초과로 테스트 실패하는 코드
 // function solution(ingredient) {
@@ -21,24 +22,86 @@
 //   return count;
 // }
 
+// 객체로 만든 다음
+// 1. 1이 나오는 경우
+// - 현재 진행 중인 객체의 문자열의 끝 문자가 3인지 확인함
+// - 3인 경우, count 개수를 증가시키고 현재 진행 중인 객체도 1 증가시킴
+// - 3이 아닌 경우, 객체에 키를 추가하여 그곳에 1을 넣음
+// 2. 2가 나오는 경우
+// - 현재 진행 중인 객체의 문자열의 끝 문자가 1인지 확인함
+// - 1인 경우, 현재 진행 중인 객체의 문자열의 끝 부분에 2를 추가함
+// - 1이 아닌 경우, 그냥 넘어감
+// 3. 3이 나오는 경우
+// - 현재 진행 중인 객체의 문자열의 끝 문자가 2인지 확인함
+// - 2인 경우, 현재 진행 중인 객체의 문자열의 끝 부분에 3을 추가함
+// - 2가 아닌 경우, 그냥 넘어감
+
 function solution(ingredient) {
-  let str = ingredient.join("");
+  const obj = {};
+  let objNum = (curObj = 1);
+  let count = 0;
 
-  const find1 = new RegExp("1231", "g");
-  const find1Arr = str.match(find1) || [];
-
-  const regex1 = /1231| /;
-  const regexStr = regex1.source;
-  const find2 = new RegExp(`1${regexStr}2${regexStr}3${regexStr}1`, "g");
-  const find2Arr = str.match(find2) || [];
-
-  const regex2 = /1(1231|)2(1231|)3(1231|)1/;
-  const regexStr2 = regex2.source;
-  const find3 = new RegExp(`1${regexStr2}2${regexStr2}3${regexStr2}1`, "g");
-  const find3Arr = str.match(find3) || [];
-
-  return find1Arr.length + find2Arr.length + find3Arr.length;
+  ingredient.forEach((num) => {
+    if (curObj <= 0) curObj = objNum + 1;
+    switch (num) {
+      case 1:
+        if (!obj[curObj]) {
+          obj[curObj] = "1";
+        } else if (obj[curObj][obj[curObj].length - 1] === "3") {
+          count++;
+          objNum = curObj;
+          curObj--;
+        } else {
+          curObj = objNum + 1;
+          obj[curObj] = "1";
+        }
+        break;
+      case 2:
+        if (!obj[curObj]) break;
+        else if (obj[curObj][obj[curObj].length - 1] === "1")
+          obj[curObj] += "2";
+        else count ? curObj-- : curObj++;
+        break;
+      case 3:
+        if (!obj[curObj]) break;
+        else if (obj[curObj][obj[curObj].length - 1] === "2")
+          obj[curObj] += "3";
+        else count ? curObj-- : curObj++;
+        break;
+    }
+  });
+  return count;
 }
+
+// objNum | curObj | count | obj
+// 0        1        0       1:
+// 0        1        0       1: "1"
+// 1        2        0       2: "1"
+// 1        2        0       2: "12"
+// 1        2        0       2: "123"
+// 3        1        1       1: "1"
+// 3        1        1       1: "12"
+// 3        1        1       1: "123"
+// 3        1        2
+
+// function solution(ingredient) {
+//   let str = ingredient.join("");
+
+//   const find1 = new RegExp("1231", "g");
+//   const find1Arr = str.match(find1) || [];
+
+//   const regex1 = /1231|  /;
+//   const regexStr = regex1.source;
+//   const find2 = new RegExp(`1${regexStr}2${regexStr}3${regexStr}1`, "g");
+//   const find2Arr = str.match(find2) || [];
+
+//   const regex2 = /1(1231|)2(1231|)3(1231|)1/;
+//   const regexStr2 = regex2.source;
+//   const find3 = new RegExp(`1${regexStr2}2${regexStr2}3${regexStr2}1`, "g");
+//   const find3Arr = str.match(find3) || [];
+
+//   return find1Arr.length + find2Arr.length + find3Arr.length;
+// }
 
 let result = solution([2, 1, 1, 2, 3, 1, 2, 3, 1, 1, 2, 1, 2, 3, 1, 3, 1]);
 console.log("결과:", result);
@@ -52,3 +115,15 @@ console.log("결과2:", result2);
 // 1231 찾기 -> 없으면 결과 리턴, 있으면 결과에 찾은 개수만큼 더하고 재귀
 // 1231 중간에 1231 끼어 있거나 없거나 -> 없으면 결과 리턴, 있으면 결과에 찾은 개수만큼 더하고 재귀
 // 1231 중간에 위의 결과 끼어 있거나 없거나
+
+// 12 12 12 12 1231 31 31 1 31 31
+
+// 1: "12"
+// 2: "12"
+// 3: "12"
+// 4: "12"
+// 5: "1231"
+// 4: "1231"
+// 3: "1231"
+// 2: "1231"
+// 1: "1231"
